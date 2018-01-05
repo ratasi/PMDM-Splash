dadc8d0d7d5d284c7036612343c0f31cc3727630
ratasi <rafatarre@gmail.com>
2018-01-05 13:54:28 +0100

    Segundo commit

--- app/src/main/java/com/example/rafa/proyecto1_pmydm/SplashActivity.java
+++ app/src/main/java/com/example/rafa/proyecto1_pmydm/SplashActivity.java
@@ -2,14 +2,19 @@ package com.example.rafa.proyecto1_pmydm;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
 import java.util.*;
+
 
 /**
  * Created by Rafa on 13/11/2017.
  */
 
 public class SplashActivity extends Activity{
-        private long SPLASH_DELAY = 3000; //6 segundos
+    private static final String TAG = "LOAD ACTIVITY"; //Nos muestra el mensaje por consola cuando el Activity es cargado
+
+    private long SPLASH_DELAY = 3000; //3 segundos
 
         @Override
         protected void onCreate (Bundle savedInstanceState){
@@ -22,6 +27,7 @@ public class SplashActivity extends Activity{
                     Intent mainIntent = new Intent(getApplicationContext(),
                             MenuActivity.class);
                     startActivity(mainIntent);
+                            Log.i(TAG, "Pasando a la siguiente pantalla");
                     //Destruimos esta activity para prevenir
                     // que el usuario vuelva a este Activity presionando el boton
                     // Atras.
